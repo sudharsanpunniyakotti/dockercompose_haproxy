447631a09cf8e9b9184a1c40e1c39c957f5349cb
root <root@ip-10-0-1-245.eu-west-1.compute.internal>
2018-06-12 09:23:08 +0000

    error in php

--- website/index.php
+++ website/index.php
@@ -7,7 +7,7 @@
         <h1>Welcome to my shop</h1>
         <ul>
             <?php
-            $json = file_get_contents('http://product-service/');
+            $json = file_get_contents('http://product/');
             $obj = json_decode($json);
             $products = $obj->products;
             foreach ($products as $product) {
